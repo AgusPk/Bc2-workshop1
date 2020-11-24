f727377f73f64598cc61c42ad05f10017742d317
ombaez <ombaez@gmail.com>
2020-11-24 20:28:33 -0300

    feat: db schema

--- db/schema-test.sql
+++ db/schema-test.sql
@@ -16,17 +16,69 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 CREATE TABLE IF NOT EXISTS `User` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `userName` VARCHAR(255) NULL,
+  `firstName` VARCHAR(255) NULL,
+  `lastName` VARCHAR(255) NULL,
+  `email` VARCHAR(100) NULL,
+  `password` VARCHAR(20) NULL,
+  `role` VARCHAR(20) NULL,
   `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` (id,userName,email,passwordEncrypted,firstName,lastName)
+INSERT INTO `User` (id,userName,firstName,lastName,email,password,role)
 values
-(1,'userName');
+(1,'Pepe','Jose','Perez','pepe@jose.com','12345678','admin');
 
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+
+CREATE TABLE IF NOT EXISTS `Cart` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `userId` INT NOT NULL ,
+  `estado` ENUM('activo','cerrado'),
+  `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+  FOREIGN KEY (`userId`) REFERENCES User(`id`))
+ENGINE = InnoDB;
+
+LOCK TABLES `Cart` WRITE;
+INSERT INTO `Cart` (id,userId,estado)
+values
+(1,1,'activo');
+
+UNLOCK TABLES;
+
+CREATE TABLE IF NOT EXISTS `Product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `categoria` VARCHAR(255) NOT NULL,
+  `nombre` VARCHAR(255) NOT NULL,
+  `cantidad` INT NULL,
+  `descripcion` VARCHAR(100) NULL,
+  `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+LOCK TABLES `Product` WRITE;
+INSERT INTO `Product` (id,categoria,nombre,cantidad,descripcion)
+values
+(1,'Perfumeria','Axe',2,'Desodorante');
+
+UNLOCK TABLES;
+
+CREATE TABLE IF NOT EXISTS `CartProduct` (
+  `cartId` INT NOT NULL,
+  `productId` INT NOT NULL,
+  FOREIGN KEY (`cartId`) REFERENCES Cart(`id`),
+  FOREIGN KEY (`productId`) REFERENCES Product(`id`),
+  PRIMARY KEY (`cartId`,`productId`),
+  )
+ENGINE = InnoDB;
+
+LOCK TABLES `CartProduct` WRITE;
+INSERT INTO `CartProduct` (cartId,productId)
+values
+(1,1);
+
 UNLOCK TABLES;
 
 SET SQL_MODE=@OLD_SQL_MODE;
