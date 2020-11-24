0d45a3a142565251709b3a8bd23667cef9d21402
ombaez <ombaez@gmail.com>
2020-11-24 20:29:15 -0300

    feat: add test router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const homeController = require('./controllers/home.controller');
+const express = require("express");
+const homeController = require("./controllers/home.controller");
+const productController = require("./controllers/product.controller");
 const router = express.Router();
 
 router.get(`/home`, homeController.get);
+router.get(`/test`, productController.getProduct);
 
-module.exports = {router};
+module.exports = { router };
