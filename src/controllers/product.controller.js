07252cc51d8aea8e797d873f388869856c9a1b76
Agustin Perkins <aperkins@octopus.com.ar>
2020-11-26 10:40:59 -0300

    product controller pt 1

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,6 +10,17 @@ class productController {
       return res.status(500).send(error);
     }
   }
+
+  static async showProduct(req, res) {
+    try {
+      const product = await productService.showProduct();
+      return res.status(200).send(product);
+    } catch (error) {
+      let status = error.notFound ? 404 : 500
+      return res.status(status).send(error);
+    }
+  }
+  
   static async createProduct(req, res) {
     try {
       const products = await productService.createProduct();
