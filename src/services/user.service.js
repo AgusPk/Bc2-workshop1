ce8cf274a00d2f9cb07abd5ec2dbfcf66564692f
Rober Villa <robertovilal2102@gmail.com>
2020-11-26 18:26:45 -0300

    add user services, bcrypt and hash-mathc functions for create user password

---
+++ src/services/user.service.js
@@ -0,0 +1,70 @@
+const userDao = require("../daos/user.dao");
+const { encryptPassword, hashPassword } = require("../utils/encrypt-hash");
+
+const services = {
+  getAll: async () => {
+    return userDao.get();
+  },
+
+  getOne: async (id) => {
+    const exist = await userDao.exists(id);
+    if (exist == 0) {
+      throw {
+        status: 404,
+        message: "User not found",
+      };
+    }
+
+    return userDao.getOne(id);
+  },
+
+  create: async ({ userName, firstName, lastName, email, password, role }) => {
+    const result = await userDao.exists(email, "email");
+    const exists = result[0].exists;
+
+    if (exists > 0) {
+      throw {
+        status: 400,
+        message: "Email is already in use, try again",
+      };
+    }
+
+    // encryptamos la password
+    const passwordEncrypted = await encryptPassword(password);
+
+    const user = [
+      email,
+      passwordEncrypted,
+      userName,
+      firstName,
+      lastName,
+      role,
+    ];
+
+    return userDao.signUp(user);
+  },
+
+  delete: async (id) => {
+    const exists = await userDao.exists(id, "id");
+    if (exists[0].exists === 0)
+      throw {
+        status: 404,
+        message: "Usuario no encontrado",
+      };
+
+    return userDao.delete(id);
+  },
+
+  update: async (id, body) => {
+    const exists = await userDao.exists(id, "id");
+    if (exists[0].exists === 0)
+      throw {
+        status: 404,
+        message: "Usuario no encontrado",
+      };
+
+    return userDao.update(id, body);
+  },
+};
+
+module.exports = services;
