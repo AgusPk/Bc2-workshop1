d0d825102bc4aec2cefc30603b728359e0231471
Agustin Perkins <aperkins@octopus.com.ar>
2020-11-26 10:40:52 -0300

    product service

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -2,12 +2,36 @@ const productDao = require("../daos/product.dao");
 
 class productService {
   static async getProduct() {
-    const result = await productDao.getProduct();
+    const result = await productDao.get();
+    return result;
+  }
+
+  static async showProduct(id) {
+    const exists = await productDao.exists(id, "id");
+    if (exists[0].exists === 0)
+      throw {
+        notFound: true,
+        error: "product_not_found",
+        msg: "Producto no encontrada",
+      };
+    const result = await productDao.show();
+    return result;
+  }
+
+  static async deleteProduct(id) {
+    const exists = await productDao.exists(id, "id");
+    if (exists[0].exists === 0)
+      throw {
+        notFound: true,
+        error: "product_not_found",
+        msg: "Producto no encontrada",
+      };
+    const result = await productDao.delete();
     return result;
   }
 
   static async createProduct() {
-    const result = await productDao.createProduct();
+    const result = await productDao.create();
     return result;
   }
 }
